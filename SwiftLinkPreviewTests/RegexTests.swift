412e69c3762b9a1c1d1032082e1c2950ed7bd257
Leonardo <leo@leocardz.com>
2016-07-16 15:36:17 -0300

    Canonical URL tests

--- SwiftLinkPreviewTests/RegexTests.swift
+++ SwiftLinkPreviewTests/RegexTests.swift
@@ -17,16 +17,31 @@ class RegexTests: XCTestCase {
     let slp = SwiftLinkPreview()
     
     // MARK: - Functions
-    func testRegex() {
+    func testURL() {
         
         for url in URLs.bunch {
             
-            slp.text = url[0]
+            self.slp.text = url[0]
             let extracted = slp.extractURL()
             
             // print(extracted?.absoluteString, url[1])
-    
+            
             XCTAssertEqual(extracted?.absoluteString, url[1])
+            
+        }
+        
+    }
+    
+    func testCanonicalURL() {
+        
+        for url in URLs.bunch {
+            
+            self.slp.result["finalUrl"] = NSURL(string: url[1])
+            self.slp.extractCanonicalURL()
+            
+            // print(self.slp.result["canonicalUrl"], url[2])
+            
+            XCTAssertEqual((self.slp.result["canonicalUrl"] as! String), url[2])
             
         }
         
--- SwiftLinkPreviewTests/URLs.swift
+++ SwiftLinkPreviewTests/URLs.swift
@@ -11,88 +11,118 @@ import Foundation
 struct URLs {
     
     // Please add the text in the format:
-    // [text, expectation]
-    // ["xxx https://your.url.com/etc#something?else=true xxx", "https://your.url.com/etc#something?else=true"]
+    // [text, expectation, canonical]
+    // ["xxx https://your.url.com/etc#something?else=true xxx", "https://your.url.com/etc#something?else=true", "your.url.com"]
     static let bunch = [
         [
             "aaa www.google.com aaa",
+            "www.google.com",
             "www.google.com"
         ],
         [
             "bbb https://www.google.com.br/?gfe_rd=cr&ei=iVKKV7nXLcSm8wfE5InADg&gws_rd=ssl bbb",
-            "https://www.google.com.br/?gfe_rd=cr&ei=iVKKV7nXLcSm8wfE5InADg&gws_rd=ssl"
+            "https://www.google.com.br/?gfe_rd=cr&ei=iVKKV7nXLcSm8wfE5InADg&gws_rd=ssl",
+            "www.google.com.br"
         ],
         [
-            "ccc123 http://www.google.com qwqwe",
-            "http://www.google.com"
+            "ccc123 http://google.com qwqwe",
+            "http://google.com",
+            "google.com"
         ],
         [
             "ddd http://ios.leocardz.com/swift-link-preview/ ddd",
-            "http://ios.leocardz.com/swift-link-preview/"
+            "http://ios.leocardz.com/swift-link-preview/",
+            "ios.leocardz.com"
+        ],
+        [
+            "ddd ios.leocardz.com/swift-link-preview/ ddd",
+            "ios.leocardz.com/swift-link-preview/",
+            "ios.leocardz.com"
         ],
         [
             "ddd ios.leocardz.com ddd",
+            "ios.leocardz.com",
             "ios.leocardz.com"
         ],
         [
             "eee http://www.nasa.gov/ eee",
-            "http://www.nasa.gov/"
+            "http://www.nasa.gov/",
+            "www.nasa.gov"
         ],
         [
             "fff theverge.com/2016/6/21/11996280/tesla-offer-solar-city-buy fff",
-            "theverge.com/2016/6/21/11996280/tesla-offer-solar-city-buy"
+            "theverge.com/2016/6/21/11996280/tesla-offer-solar-city-buy",
+            "theverge.com"
+        ],
+        [
+            "fff http://theverge.com/2016/6/21/11996280/tesla-offer-solar-city-buy fff",
+            "http://theverge.com/2016/6/21/11996280/tesla-offer-solar-city-buy",
+            "theverge.com"
         ],
         [
             "ggg http://bit.ly/14SD1eR ggg",
-            "http://bit.ly/14SD1eR"
+            "http://bit.ly/14SD1eR",
+            "bit.ly"
         ],
         [
             "hhh https://twitter.com hhh",
-            "https://twitter.com"
+            "https://twitter.com",
+            "twitter.com"
         ],
         [
             "hhh twitter.com hhh",
+            "twitter.com",
             "twitter.com"
         ],
         [
             "iii https://www.nationalgallery.org.uk#2123123?sadasd&asd iii",
-            "https://www.nationalgallery.org.uk#2123123?sadasd&asd"
+            "https://www.nationalgallery.org.uk#2123123?sadasd&asd",
+            "www.nationalgallery.org.uk"
         ],
         [
             "jjj http://globo.com jjj",
-            "http://globo.com"
+            "http://globo.com",
+            "globo.com"
         ],
         [
             "kkk http://uol.com.br kkk",
-            "http://uol.com.br"
+            "http://uol.com.br",
+            "uol.com.br"
         ],
         [
             "lll http://vnexpress.net/ lll",
-            "http://vnexpress.net/"
+            "http://vnexpress.net/",
+            "vnexpress.net"
         ],
         [
             "mmm http://www3.nhk.or.jp/ mmm",
-            "http://www3.nhk.or.jp/"
+            "http://www3.nhk.or.jp/",
+            "www3.nhk.or.jp"
         ],
         [
             "nnn http://habrahabr.ru nnn",
-            "http://habrahabr.ru"
+            "http://habrahabr.ru",
+            "habrahabr.ru"
         ],
         [
             "ooo http://www.youtube.com/watch?v=cv2mjAgFTaI ooo",
-            "http://www.youtube.com/watch?v=cv2mjAgFTaI"
+            "http://www.youtube.com/watch?v=cv2mjAgFTaI",
+            "www.youtube.com"
         ],
         [
             "ppp http://vimeo.com/67992157 ppp",
-            "http://vimeo.com/67992157"
+            "http://vimeo.com/67992157",
+            "vimeo.com"
         ],
         [
             "qqq https://lh6.googleusercontent.com/-aDALitrkRFw/UfQEmWPMQnI/AAAAAAAFOlQ/mDh1l4ej15k/w337-h697-no/db1969caa4ecb88ef727dbad05d5b5b3.jpg qqq",
-            "https://lh6.googleusercontent.com/-aDALitrkRFw/UfQEmWPMQnI/AAAAAAAFOlQ/mDh1l4ej15k/w337-h697-no/db1969caa4ecb88ef727dbad05d5b5b3.jpg"
+            "https://lh6.googleusercontent.com/-aDALitrkRFw/UfQEmWPMQnI/AAAAAAAFOlQ/mDh1l4ej15k/w337-h697-no/db1969caa4ecb88ef727dbad05d5b5b3.jpg",
+            "lh6.googleusercontent.com"
         ],
         [
             "rrr http://goo.gl/jKCPgp rrr",
-            "http://goo.gl/jKCPgp"
+            "http://goo.gl/jKCPgp",
+            "goo.gl"
         ]
     ]
     
