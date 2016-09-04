c4f3600d9d8cae99cfc28427e7c36f72a47f4e2e
Daniel Rhodes <rhodes.daniel@gmail.com>
2016-09-04 22:10:30 +0200

    PreviewError conforms to ErrorType
    * Removes PreviewErrorTypes
    * Moves functionality of PreviewErrorTypes to PreviewError
    * PreviewError is now an enum which takes a string argument for additional information.
    
    No user-facing API changes.

--- Sources/PreviewError.swift
+++ Sources/PreviewError.swift
@@ -7,24 +7,18 @@
 //
 import Foundation
 
-// MARK: - Error enum
-public class PreviewError {
-    
-    public var message: String?
-    public var type: PreviewErrorType?
-    
-    public init(type: PreviewErrorType, url: NSURL) {
-        
-        self.type = type
-        self.message = type.rawValue + ": \"\(url.absoluteString)\""
-    
+public enum PreviewError: ErrorType, CustomStringConvertible {
+  case NoURLHasBeenFound(String?)
+  case InvalidURL(String?)
+  case CannotBeOpened(String?)
+  case ParseError(String?)
+  
+  public var description: String {
+    switch(self) {
+      case .NoURLHasBeenFound: return NSLocalizedString("No URL has been found", comment: "")
+      case .InvalidURL: return NSLocalizedString("This data is not valid URL", comment: "")
+      case .CannotBeOpened: return NSLocalizedString("This URL cannot be opened", comment: "")
+      case .ParseError: return NSLocalizedString("An error occurred when parsing the HTML", comment: "")
     }
-    
-    public init(type: PreviewErrorType, url: String){
-        
-        self.type = type
-        self.message = type.rawValue + ": \"\(url)\""
-        
-    }
-    
+  }
 }
