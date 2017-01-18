e42c905359c15795b842acde97ff9bb18c5e608e
Yehor Popovych <y.popovych@crossroadlabs.xyz>
2017-01-18 05:36:30 +0200

    Internal queue for InMemory cache (thread safety)

--- Sources/Cache.swift
+++ Sources/Cache.swift
@@ -30,9 +30,13 @@ public class InMemoryCache: Cache {
     private let invalidationTimeout: TimeInterval
     private let cleanupTimer: DispatchSource
     
+    //High priority queue for quick responses
+    private static let cacheQueue = DispatchQueue(label: "SwiftLinkPreviewInMemoryCacheQueue", qos: .userInitiated, target: DispatchQueue.global(qos: .userInitiated))
+    
     public init(invalidationTimeout: TimeInterval = 300.0) {
         self.invalidationTimeout = invalidationTimeout
-        self.cleanupTimer = DispatchSource.makeTimerSource(queue: DispatchQueue.main) as! DispatchSource
+        
+        self.cleanupTimer = DispatchSource.makeTimerSource(queue: type(of:self).cacheQueue) as! DispatchSource
         self.cleanupTimer.scheduleRepeating(deadline: .now() + invalidationTimeout / 3, interval: invalidationTimeout / 3)
         
         self.cleanupTimer.setEventHandler { [weak self] in
@@ -44,30 +48,32 @@ public class InMemoryCache: Cache {
     }
     
     public func cleanup() {
-        SwiftLinkPreview.defaultWorkQueue.async {
-            for (url, data) in self.cache {
-                if data.date.timeIntervalSinceNow > self.invalidationTimeout {
-                    self.cache[url] = nil
-                }
+        for (url, data) in cache {
+            if data.date.timeIntervalSinceNow > invalidationTimeout {
+                cache[url] = nil
             }
         }
     }
     
     public func slp_getCachedResponse(url: String) -> SwiftLinkPreview.Response? {
-        guard let response = cache[url] else { return nil }
-        
-        if response.date.timeIntervalSinceNow >= invalidationTimeout {
-            slp_setCachedResponse(url: url, response: nil)
-            return nil
+        return type(of: self).cacheQueue.sync {
+            guard let response = cache[url] else { return nil }
+            
+            if response.date.timeIntervalSinceNow >= invalidationTimeout {
+                slp_setCachedResponse(url: url, response: nil)
+                return nil
+            }
+            return response.response
         }
-        return response.response
     }
     
     public func slp_setCachedResponse(url: String, response: SwiftLinkPreview.Response?) {
-        if let response = response {
-            cache[url] = (response, Date())
-        } else {
-            cache[url] = nil
+        type(of: self).cacheQueue.sync {
+            if let response = response {
+                cache[url] = (response, Date())
+            } else {
+                cache[url] = nil
+            }
         }
     }
     
