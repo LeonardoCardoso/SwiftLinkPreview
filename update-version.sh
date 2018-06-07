6c3c4abdf0c98ef00be4814889f2ee3e50af7a9e
Leonardo <leo@leocardz.com>
2018-06-07 16:43:44 +0200

    Bump to version 2.3.0

--- update-version.sh
+++
@@ -1,3 +0,0 @@
-grep -rl '2\.1\.0' .  --exclude-dir={"Build","libs","Pods",".git"} --exclude={"Podfile.lock","CHANGELOG.md","update-version.sh"} | xargs sed -i.bak 's/2\.1\.0/2\.2\.0/g';
-
-find . -type f -name '*.bak' -delete
