8db88b04232dba58d09720c6bc90775c1cadd4fb
Leonardo <leo@leocardz.com>
2018-06-07 15:22:32 +0200

    task: Update example

--- Example/SwiftLinkPreviewExample/Controllers/ViewController.swift
+++ Example/SwiftLinkPreviewExample/Controllers/ViewController.swift
@@ -31,30 +31,34 @@ class ViewController: UIViewController {
     
     // MARK: - Vars
     private var randomTexts: [String] = [
-        "https://www.linkedin.com/",
-        "www.youtube.com",
-        "www.google.com",
-        "facebook.com",
-
-        "https://leocardz.com/swift-link-preview-5a9860c7756f",
-        "NASA! 🖖🏽 https://www.nasa.gov/",
-        "https://www.theverge.com/2016/6/21/11996280/tesla-offer-solar-city-buy",
-        "Shorten URL http://bit.ly/14SD1eR",
-        "Tweet! https://twitter.com",
-
-        "A Gallery https://www.nationalgallery.org.uk",
-
-        "A Brazilian website http://globo.com",
-        "Another Brazilian website https://uol.com.br",
-        "Some Vietnamese chars https://vnexpress.net/",
-        "Japan!!! https://www3.nhk.or.jp/",
-        "A Russian website >> https://habrahabr.ru",
-
-        "Youtube?! It does! https://www.youtube.com/watch?v=cv2mjAgFTaI",
-        "Also Vimeo https://vimeo.com/67992157",
-
-        "Even with image itself https://lh6.googleusercontent.com/-aDALitrkRFw/UfQEmWPMQnI/AAAAAAAFOlQ/mDh1l4ej15k/w337-h697-no/db1969caa4ecb88ef727dbad05d5b5b3.jpg",
-        "Well, it's a gif! https://goo.gl/jKCPgp",
+//        "https://www.monicapackpilates.com/",
+//        "https://www.yourgesture.com/",
+//        "http://www.adammccain.org/",
+//        "https://www.linkedin.com/",
+//        "www.youtube.com",
+//        "www.google.com",
+//        "facebook.com",
+//
+//        "https://leocardz.com/swift-link-preview-5a9860c7756f",
+//        "NASA! 🖖🏽 https://www.nasa.gov/",
+//        "https://www.theverge.com/2016/6/21/11996280/tesla-offer-solar-city-buy",
+//        "Shorten URL http://bit.ly/14SD1eR",
+//        "Tweet! https://twitter.com",
+//
+//        "A Gallery https://www.nationalgallery.org.uk",
+        "www.dji.com/matrice600-pro/info#specs",
+//
+//        "A Brazilian website http://globo.com",
+//        "Another Brazilian website https://uol.com.br",
+//        "Some Vietnamese chars https://vnexpress.net/",
+//        "Japan!!! https://www3.nhk.or.jp/",
+//        "A Russian website >> https://habrahabr.ru",
+//
+//        "Youtube?! It does! https://www.youtube.com/watch?v=cv2mjAgFTaI",
+//        "Also Vimeo https://vimeo.com/67992157",
+//
+//        "Even with image itself https://lh6.googleusercontent.com/-aDALitrkRFw/UfQEmWPMQnI/AAAAAAAFOlQ/mDh1l4ej15k/w337-h697-no/db1969caa4ecb88ef727dbad05d5b5b3.jpg",
+//        "Well, it's a gif! https://goo.gl/jKCPgp",
     ]
     private var result = SwiftLinkPreview.Response()
     private let placeholderImages = [ImageSource(image: UIImage(named: "Placeholder")!)]
@@ -270,13 +274,15 @@ class ViewController: UIViewController {
             
             self.result = cached
             self.setData()
+
+            result.forEach { print("\($0):", $1) }
             
         } else {
             self.slp.preview(
                 textFieldText,
                 onSuccess: { result in
                     
-                    print(result)
+                    result.forEach { print("\($0):", $1) }
                     self.result = result
                     self.setData()
                     
