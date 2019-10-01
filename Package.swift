020f17d51f684cf022ff9de267ada19b9a1d3f79
Spencer Schoeben <spencer@netspencer.com>
2019-10-01 12:33:38 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,13 +1,15 @@
-//
-//  Package.swift
-//  SwiftLinkPreview
-//
-//  Created by Leonardo Cardoso on 04/07/2016.
-//  Copyright © 2016 leocardz.com. All rights reserved.
-//
-
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-    name: "SwiftLinkPreview"
+    name: "SwiftLinkPreview",
+    platforms: [
+        .iOS(.v8),
+    ],
+    products: [
+        .library(name: "SwiftLinkPreview", targets: ["SwiftLinkPreview"]),
+    ],
+    targets: [
+        .target(name: "SwiftLinkPreview", path: "Sources")
+    ]
 )
