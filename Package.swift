6a741186db8ae79d4ad141ae3da57ac997a0c271
Eric Loew <ericloew@hotmail.com>
2020-07-24 03:26:47 -0500

    before merge

--- Package.swift
+++ Package.swift
@@ -1,11 +1,4 @@
-//
-//  Package.swift
-//  SwiftLinkPreview
-//
-//  Created by Leonardo Cardoso on 04/07/2016.
-//  Copyright © 2016 leocardz.com. All rights reserved.
-//
-
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
